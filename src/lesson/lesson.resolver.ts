351f396ac35e2601b1661c663725ae70b710adb2
francisco.abalan <francisco.abalan@agranimo.com>
2021-06-25 17:27:14 -0400

    development findOne and all lessons feature

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,31 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { LessonService } from "./lesson.service";
 import { LessonType } from "./lesson.type";
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(
+    private lessonService: LessonService
+  ){}
+
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'qwerty',
-      name: 'Calc II',
-      startDate: (new Date()).toISOString(),
-      endDate: (new Date()).toISOString()
-    };
+  lesson(
+    @Args('id') id: string,
+  ) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query(returns => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
